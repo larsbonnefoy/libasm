ea5aceb763a05003d241cbc7d44af6f18aac1dad
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-09-07 17:17:15 +0200

    fixed ft_strlen

--- lib/ft_strlen.s
+++ lib/ft_strlen.s
@@ -3,12 +3,14 @@ section .text
 global __ft_strlen              ;global defines symbols
 
 __ft_strlen:
+    xor rax, rax
+    jmp .loop
 
-mov rax, -1
+.incr:
+    inc rax
 
 .loop:                          ;counting loop 
-inc rax                         ;inc rax counter by 1
-cmp byte [rdi + rax], 0         ;checks if rax byte = 0
-jne .loop                       ;if cmp not equal to 1 loop back
+    cmp byte [rdi + rax], 0         ;checks if rax byte = 0
+    jne .incr                       ;if cmp not equal to 1 loop back
 
 ret                             ;return value contained in rax
--- lib/main.c
+++ lib/main.c
@@ -4,29 +4,37 @@
 #include <stdlib.h>
 
 
-size_t ft_strlenC(const char *s);
+size_t ft_strlen_c(const char *s);
 extern size_t _ft_strlen(const char *s);
 
-char *ft_strcpyC(char *dst, const char *src);
+char *ft_strcpy_c(char *dst, const char *src);
 extern char *_ft_strcpy(char *dst, const char *src);
+
+int ft_strcmp_c(const char *s1, const char *s2);
 
 int main(int argc, char *argv[])
 {
-    char *s = "Hello World";
+    const char *s = "Hello World";
     printf("%lu\n", strlen(s));
-    printf("%lu\n", ft_strlenC(s));
+    printf("%lu\n", ft_strlen_c(s));
     printf("%lu\n", _ft_strlen(s));
 
 
-    s = "Hello World in strcpy";
+    const char *s1 = "Hello World in strcpy";
     char dst[256];
-    printf("%s | %s\n", strcpy(dst, s), dst);
-    printf("%s | %s\n", ft_strcpyC(dst, s), dst);
-    printf("%s | %s\n", _ft_strcpy(dst, s), dst);
+    printf("%s | %s\n", strcpy(dst, s1), dst);
+    printf("%s | %s\n", ft_strcpy_c(dst, s1), dst);
+    printf("%s | %s\n", _ft_strcpy(dst, s1), dst);
+
+    const char *s2 = "Hello World";
+    const char *s3 = "meep";
+    const char *s4 = "Hello World but not the same";
+    printf("%d, %d, %d, %d\n", strcmp(s,s), strcmp(s, s2), strcmp(s, s3), strcmp(s, s4));
+    printf("%d, %d, %d, %d\n", ft_strcmp_c(s,s), ft_strcmp_c(s, s2), ft_strcmp_c(s, s3), ft_strcmp_c(s, s4));
     return EXIT_SUCCESS;
 }
 
-size_t ft_strlenC(const char *s) {
+size_t ft_strlen_c(const char *s) {
     
     size_t i = 0;
 
@@ -37,7 +45,7 @@ size_t ft_strlenC(const char *s) {
     return (i);
 }
 
-char *ft_strcpyC(char *dst, const char *src) {
+char *ft_strcpy_c(char *dst, const char *src) {
     size_t i = 0;
 
     while (src[i] != 0) {
@@ -46,4 +54,16 @@ char *ft_strcpyC(char *dst, const char *src) {
     }
     dst[i] = 0;
     return dst;
+}
+
+int ft_strcmp_c(const char *s1, const char *s2) {
+    size_t i = 0;
+
+    while (s1[i] == s2[i]) {
+        if (s1[i] == 0 || s2[i] ==  0) {                //-> if these lines ar not present we have a bus error
+            return 0;
+        }
+        i++;
+    }
+    return (s1[i] - s2[i]);
 }
