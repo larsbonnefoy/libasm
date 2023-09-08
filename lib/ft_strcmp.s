cd37524bbf4dc9a2353bcf193b913960f13351be
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-09-08 10:53:42 +0200

    switched sign extension intstruction

--- lib/ft_strcmp.s
+++ lib/ft_strcmp.s
@@ -11,13 +11,12 @@ _ft_strcmp:
 .loop:
 	inc rcx
 	mov al, byte [rdi + rcx] 		;mov s1[i] into al
-	cmp al, 0
+	cmp al, 0						;check for null terminating byte
 	je .end
 	cmp byte [rsi + rcx], al		;cmp  s2[i] with al
 	je .loop						;if s1[i] == s2[i] loop
 									;-> should check if s1[i] == 0 || s2[i] == 0
 .end:
-	sub al, byte [rsi + rcx]		;s1[i] - s2[i] -> return int not byte => make a signed conversion
-	cbw
-	cwde
+	sub al, byte [rsi + rcx]		;s1[i] - s2[i]
+	movsx eax, al					;sign extend al to eax to return 32 bit value
 	ret
